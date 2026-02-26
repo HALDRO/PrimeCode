dc3c5808cc3550ff093d17cf1353a7849b8499b4
HALDRO <121296348+HALDRO@users.noreply.github.com>
2026-02-26 15:45:53 +0800

    Delete old Docs

--- docs/opencode-gui-main/src/webview/utils/vscode.ts
+++
@@ -1,9 +0,0 @@
-declare const acquireVsCodeApi: (() => { postMessage: (message: unknown) => void }) | undefined;
-
-export const hasVscodeApi = typeof acquireVsCodeApi !== "undefined";
-
-const noopVscode = {
-  postMessage: (_message: unknown) => {},
-};
-
-export const vscode = hasVscodeApi ? acquireVsCodeApi!() : noopVscode;
